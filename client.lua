ea2d0e7373a36d13af14053aeeba9544372470a0
cadburry <72443203+cadburry6969@users.noreply.github.com>
2021-12-28 23:23:48 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,67 +1,14 @@
--- ================================================
---                 EVENTS
--- ================================================
 RegisterNetEvent("vehwipe:client:delallveh")
 AddEventHandler("vehwipe:client:delallveh", function ()
-    for vehicle in EnumerateVehicles() do
-        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
-            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
-            SetEntityAsMissionEntity(vehicle, false, false) 
-            DeleteVehicle(vehicle)
-            if (DoesEntityExist(vehicle)) then 
-                DeleteVehicle(vehicle) 
+        local vehicles = GetGamePool('CVehicle')
+    for i=1, #vehicles, 1 do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicles[i], -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(vehicles[i], false) 
+            SetEntityAsMissionEntity(vehicles[i], false, false) 
+            DeleteVehicle(vehicles[i])
+            if (DoesEntityExist(vehicles[i])) then 
+                DeleteVehicle(vehicles[i]) 
             end
         end
     end
 end)
-
--- ================================================
---               EnumerateEntities
--- ================================================
-local entityEnumerator = {
-    __gc = function(enum)
-      if enum.destructor and enum.handle then
-        enum.destructor(enum.handle)
-      end
-      enum.destructor = nil
-      enum.handle = nil
-    end
-  }
-  
-  local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
-    return coroutine.wrap(function()
-      local iter, id = initFunc()
-      if not id or id == 0 then
-        disposeFunc(iter)
-        return
-      end
-      
-      local enum = {handle = iter, destructor = disposeFunc}
-      setmetatable(enum, entityEnumerator)
-      
-      local next = true
-      repeat
-        coroutine.yield(id)
-        next, id = moveFunc(iter)
-      until not next
-      
-      enum.destructor, enum.handle = nil, nil
-      disposeFunc(iter)
-    end)
-  end
-  
-  function EnumerateObjects()
-    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
-  end
-  
-  function EnumeratePeds()
-    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
-  end
-  
-  function EnumerateVehicles()
-    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
-  end
-  
-  function EnumeratePickups()
-    return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
-  end
