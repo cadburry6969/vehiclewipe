294ced47b04bc1c2c0078090503e600241a6f464
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-11-21 16:33:55 +0530

    cleanup

--- server.lua
+++ server.lua
@@ -2,27 +2,23 @@
 --                 COMMAND
 -- ================================================
 RegisterCommand("vehwipe", function(source, args)
-	local src = source
-    if IsPlayerAceAllowed(src, "command") then
-    	TriggerClientEvent("vehwipe:client:delallveh", -1)
-		end
-end)
+    TriggerClientEvent("vehiclewipe:delallveh", -1)
+end, true)
 
 -- ================================================
 --                AUTO WIPE
 -- ================================================
 if Config.AutoWipe then 
-    Citizen.CreateThread(function()       
-        while true do                    
-            Citizen.Wait((1000 * 60) * Config.Timer)
+    CreateThread(function()
+        while true do
+	    Wait((1000 * 60) * Config.Timer)
             if Config.ChatMessages then
 	        TriggerClientEvent('chat:addMessage', -1, {template = '<div class="chat-message emergency"><r> {0}</r> {1}</div>', args = { "SYSTEM:", "Vehicle Wipe Incoming in "..Config.WarningTimer.." Min" } })
-                Citizen.Wait((1000 * 60) * Config.WarningTimer)                                                                             
-                TriggerClientEvent("vehwipe:client:delallveh", -1)
+                Wait((1000 * 60) * Config.WarningTimer)                                                                             
+                TriggerClientEvent("vehiclewipe:delallveh", -1)
             else
-                TriggerClientEvent("vehwipe:client:delallveh", -1)
-            end            
-            Citizen.Wait(0)
+                TriggerClientEvent("vehiclewipe:delallveh", -1)
+            end
         end
     end)
 end
