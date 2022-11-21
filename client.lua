f4c9d3f83c37be39397d8bca5cfaa2399a9be253
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-11-21 16:31:47 +0530

    feat: vehwipe loop or delete if empty

--- client.lua
+++ client.lua
@@ -1,14 +1,34 @@
-RegisterNetEvent("vehwipe:client:delallveh")
-AddEventHandler("vehwipe:client:delallveh", function ()
-        local vehicles = GetGamePool('CVehicle')
+local function CheckAndDeleteVehicle(vehicle)
+    if DoesEntityExist(vehicle) then
+        if (GetVehicleNumberOfPassengers(vehicle) == 0) and IsVehicleSeatFree(Vehicle, -1) then
+            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
+            SetEntityAsMissionEntity(vehicle, false, false) 
+            DeleteEntity(vehicle)
+        end
+    end
+end
+
+local function DeleteVehicleLoop()
+    local vehPool = GetGamePool('CVehicle')
+    for i = 1, #vehPool do
+        CheckAndDeleteVehicle(vehPool[i])
+    end
+    SetTimeout(20000, DeleteVehicleLoop)
+end
+
+DeleteVehicleLoop()
+
+AddEventHandler("baseevents:leftVehicle", function(curVeh, curSeat, vehName)
+    if curSeat == -1 then
+        CheckAndDeleteVehicle(curVeh)
+    end
+end)
+
+RegisterNetEvent("vehiclewipe:delallveh", function ()
+    local vehicles = GetGamePool('CVehicle')
     for i=1, #vehicles, 1 do
         if (not IsPedAPlayer(GetPedInVehicleSeat(vehicles[i], -1))) then 
-            SetVehicleHasBeenOwnedByPlayer(vehicles[i], false) 
-            SetEntityAsMissionEntity(vehicles[i], false, false) 
-            DeleteVehicle(vehicles[i])
-            if (DoesEntityExist(vehicles[i])) then 
-                DeleteVehicle(vehicles[i]) 
-            end
+            CheckAndDeleteVehicle(vehicles[i])
         end
     end
 end)
